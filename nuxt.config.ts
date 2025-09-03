520056c14b66acd31063d7f57db6025132750885
Sergey Razuvaev <serg@sv-tel.ru>
2025-09-03 06:17:36 +0300

    feat: eslint config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,21 @@
+
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
   devtools: { enabled: true },
+  css: ['~/app/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 
   modules: [
     '@nuxt/eslint',
