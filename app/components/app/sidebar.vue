447f97ae0f304164b906de220702f7abeced0dd7
sergey12313 <sergey12313@gmail.com>
2025-09-04 13:37:24 +0300

    feat: add sidebar (#4)
    Co-authored-by: Sergey Razuvaev <serg@sv-tel.ru>

---
+++ app/components/app/sidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col border-r  border-gray-200 py-7 justify-between items-center">
+    <nuxt-link to="/" class="icon-btn">
+      <icon size="24" name="app-icon:home" />
+    </nuxt-link>
+    <nuxt-link to="/" class="icon-btn">
+      <icon size="24" name="app-icon:exit" />
+    </nuxt-link>
+  </div>
+</template>
+
+<style scoped>
+@reference "tailwindcss";
+.icon-btn {
+  @apply w-9 h-9 hover:bg-gray-100 grid place-items-center rounded-full;
+}
+</style>
